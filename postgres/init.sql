6624db8fdb8834552e935f0fe71fb870d68b09aa
Weronika Pankalla <s219729@student.uek.krakow.pl>
2023-01-24 21:23:37 +0100

    third

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,8 +1,19 @@
-CREATE TABLE accounts (
-	user_id serial PRIMARY KEY,
-	username VARCHAR ( 50 ) UNIQUE NOT NULL,
-	password VARCHAR ( 50 ) NOT NULL,
+CREATE TABLE emails (
+	id serial PRIMARY KEY,
 	email VARCHAR ( 255 ) UNIQUE NOT NULL,
-	created_on TIMESTAMP NOT NULL,
-        last_login TIMESTAMP 
+    cryptocurrency VARCHAR ( 255 ) NOT NULL,
+	created_on TIMESTAMP NOT NULL
+);
+CREATE TABLE sending_status (
+    id serial PRIMARY KEY,
+    email VARCHAR ( 255 ) UNIQUE NOT NULL,
+    cryptocurrency VARCHAR ( 255 ) NOT NULL,
+    sending_time TIMESTAMP NOT NULL,
+    s_status VARCHAR ( 255 ) NOT NULL
+);
+CREATE TABLE price_history (
+    id serial PRIMARY KEY,
+    cryptocurrency VARCHAR ( 255 ) NOT NULL,
+    price MONEY NOT NULL,
+    date_of_price TIMESTAMP NOT NULL
 );
